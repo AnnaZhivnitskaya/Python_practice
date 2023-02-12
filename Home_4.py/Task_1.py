4873778c2e5d8eecfff1f9387585c7daa4075406
Anna <A9183307737@gmail.com>
2023-02-12 10:47:45 +0300

    task 1

--- Home_4.py/Task_1.py
+++ Home_4.py/Task_1.py
@@ -8,40 +8,49 @@ import os
 
 os.system("cls")
 
-n = int(input('Введите длину множества n: '))
-m = int(input('Введите длину множества m: '))
+# n = int(input('Введите длину множества n: '))
+# m = int(input('Введите длину множества m: '))
 
-set_n = set()
-for i in range(n):
-    n_1 = int(input())
-    set_n.add(n_1)
-print(set_n)
+# set_n = set()
+# for i in range(n):
+#     n_1 = int(input())
+#     set_n.add(n_1)
+# print(set_n)
 
-set_m = set()
-for i in range(m):
-    m_1 = int(input())
-    set_m.add(m_1)
-print(set_m)
+# set_m = set()
+# for i in range(m):
+#     m_1 = int(input())
+#     set_m.add(m_1)
+# print(set_m)
 
-list_1 = list(set_n.intersection(set_m))
-list_1.sort()
-print(*list_1)
+# list_1 = list(set_n.intersection(set_m))
+# list_1.sort()
+# print(*list_1)
 
 
 # n = int(input('Введите длину множества n: '))
 # m = int(input('Введите длину множества m: '))
 
 # def make_set(a):
-#     set_a = set()
+#     set_a = []
 #     for i in range(a):
 #         a_1 = int(input())
-#         set_n.add(a_1)
-#     return set_a
+#         set_a.append(a_1)
+#     return set(set_a)
 
-# set_n = set()
-# set_m = set()
-# make_set(n)
-# make_set(m)
-# list_1 = list(set_n.union(set_m))
+# set_n = make_set(n)
+# set_m = make_set(m)
+
+# list_1 = list(set_n.intersection(set_m))
 # list_1.sort()
 # print(*list_1)
+
+
+
+n, m = input().split()
+first = {int(i) for i in input().split()}
+second = {int(j) for j in input().split()}
+
+print(type(second))
+
+print(sorted(first.intersection(second)))
