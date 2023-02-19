df5cb0bfd15cda23f81bf3afd6aa8b52042163f6
Anna <A9183307737@gmail.com>
2023-02-19 23:14:45 +0300

    task 2

--- Home_6/Task_2.py
+++ Home_6/Task_2.py
@@ -27,3 +27,7 @@ max_number = int(input())
 for i in range(len(list_1)): 
     if min_number <= list_1[i] <= max_number: 
         print(i)
+
+N = list(int(i) for i in input().split())
+n_min, n_max = (int(input()) for _ in 'ab')
+print(list(i for i in range(len(N)) if N[i] in range(n_min, n_max + 1)))
